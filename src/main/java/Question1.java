e039d397ba8a2fbff96a7175a3f6c80b98e638e7
adhvitaahuja <78463191+adhvitaahuja@users.noreply.github.com>
2021-04-29 20:24:21 +0530

    Files added

---
+++ src/main/java/Question1.java
@@ -0,0 +1,72 @@
+/*
+Dataprovider to read data from excel using hashmap with Optimized code
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Question1 {
+    @Test(dataProvider = "getdata")
+    public void login(Map<Object, Object> mapdata) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys(mapdata.get("username").toString());
+        driver.findElement(By.xpath("//input[@id='passwd']")).sendKeys(mapdata.get("password").toString());
+        driver.findElement(By.xpath("//button[@id='SubmitLogin']")).click();
+        Assert.assertTrue(driver.getPageSource().contains("Welcome to your account"),"Account Not Created");
+        System.out.println("Account created successfully!!");
+        driver.close();
+    }
+    @DataProvider(name ="getdata")
+    public Object[][] dataproviderMethod() {
+        FileInputStream fis = null;
+        XSSFSheet ws = null;
+        XSSFWorkbook wb = null;
+        String filePath = "datafile.xlsx";
+        File xls = new File(filePath);
+        try {
+            fis = new FileInputStream(xls);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            wb = new XSSFWorkbook(fis);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        ws = wb.getSheet("Sheet1");
+        int rowcount = ws.getLastRowNum();
+        int colcount = ws.getRow(0).getLastCellNum();
+        Object[][] data = new Object[rowcount][1];
+        for (int r = 0; r < rowcount; r++) {
+            HashMap<Object, Object> hm = new HashMap<Object, Object>();
+            for (int c = 0; c < colcount; c++) {
+                hm.put(ws.getRow(0).getCell(c).toString(), ws.getRow(r + 1).getCell(c).toString());
+            }
+            data[r][0] = hm;
+        }
+        try {
+            wb.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+        return data;
+    }
+}
---
+++ src/main/java/Question2.java
@@ -0,0 +1,31 @@
+/*
+Read console data using selenium
+ */
+
+import java.util.List;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+
+public class Question2{
+    public static void main(String args[]){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.tothenew.com/");
+        driver.manage().window().maximize();
+        LogEntries entry = driver.manage().logs().get(LogType.BROWSER);
+        List<LogEntry> logs= entry.getAll();
+
+        System.out.println("******************* CONSOLE LOGS *******************");
+
+        for(LogEntry e: logs)
+        {
+            System.out.println("Message is: " +e.getMessage());
+            System.out.println("Level is: " +e.getLevel());
+        }
+        driver.close();
+    }
+}
